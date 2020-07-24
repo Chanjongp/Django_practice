0d51ed61e9413c81f1a1cf534a73243a9506e2f6
DESKTOP-GJBDRJT\s9420 <pcj0198@daum.net>
2020-07-24 23:29:00 +0900

    Django before form

--- firstdjango/urls.py
+++ firstdjango/urls.py
@@ -17,9 +17,9 @@ from django.contrib import admin
 from django.urls import path, include
 
 from first import views
-
+from second import views
 urlpatterns = [
-    path('first', include('first.urls')),
-    path('second', include('second.urls')),
+    path('first/', include('first.urls')),
+    path('second/', include('second.urls')),
     path('admin/', admin.site.urls),
 ]
--- second/urls.py
+++ second/urls.py
@@ -3,7 +3,6 @@ from django.urls import path
 from second import views
 
 urlpatterns = [
-
-    
+    path('list/', views.list, name="list"),
 ]
 
